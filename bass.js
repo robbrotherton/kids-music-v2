cc96d4acebc733e30789228ba21004197f42d250
Robert Brotherton <robert.brotherton@gmail.com>
2025-12-23 13:31:34 -0500

    layout

--- bass.js
+++ bass.js
@@ -8,7 +8,8 @@ window.bassTrack = {
 };
 
 // Bass notes (12-note chromatic, moved down an octave)
-window.bassNotes = ['C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1'];
+window.bassNotes = ['C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
+                    'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2'];
 
 // Initialize bass keyboard
 window.initBassKeyboard = function() {
