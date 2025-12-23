e45ac4a16e9795a9708b8a824ab30cd661d952b5
Robert Brotherton <robert.brotherton@gmail.com>
2025-12-23 13:27:50 -0500

    2 octaves of rhythm

--- rhythm.js
+++ rhythm.js
@@ -8,7 +8,8 @@ window.rhythmTrack = {
 };
 
 // Rhythm notes: choose an octave range suitable for rhythm synth (e.g., C3..B3)
-window.rhythmNotes = ['C3','C#3','D3','D#3','E3','F3','F#3','G3','G#3','A3','A#3','B3'];
+window.rhythmNotes = ['C3','C#3','D3','D#3','E3','F3','F#3','G3','G#3','A3','A#3','B3',
+                      'C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4'];
 
 // Initialize rhythm keyboard (reuse createPianoKeyboard)
 window.initRhythmKeyboard = function() {
@@ -17,7 +18,7 @@ window.initRhythmKeyboard = function() {
 
     window.createPianoKeyboard({
         container: keyboardContainer,
-        octaves: 1,
+        octaves: 2,
         startOctave: 3,
         instrument: 'rhythm',
         onKeyDown: (note, frequency) => {
